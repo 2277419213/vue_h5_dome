8c013d879845f43f727d80786e4cee68eb723b12
上班钓鱼🎣小居里 <812936565@qq.com>
2019-10-23 15:34:34 +0800

    first commit by 居里栈栈

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png" /> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+/* 解决苹果不现实图片问题 */
+img {
+  content: normal !important;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      eslint: "eslint语法不能为空"
+    };
+  },
+  watch: {},
+  methods: {},
+  mounted() {}
+}
+</script>
+<style scoped>
+</style>
